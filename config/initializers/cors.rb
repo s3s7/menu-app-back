9a4d71f3647eff018ae43f56acdc74f38a397860
shun <samesamejinbei@gmail.com>
2024-12-26 20:15:31 +0900

    first commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,7 +18,7 @@
 #
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000', '127.0.0.1:8000'
+    origins 'localhost:8000', '127.0.0.1:8000','https://menu-app-front.vercel.app'
 
     resource "*",
       headers: :any,
